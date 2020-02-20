953a11172fae14426cbddd476cf3f4c1ac59073b
Frederic Leger <frederic@webofmars.com>
2020-02-20 12:24:44 +0100

    better script logic (again)

--- sync-repos.sh
+++ sync-repos.sh
@@ -3,13 +3,13 @@
 set -e -o pipefail
 
 echo "+ synchronizing all branches"
-git merge ci/github-actions master
-git merge ci/gitlab-saas master
-git merge ci/drone master
+git merge --ff-only ci/github-actions master
+git merge --ff-only ci/gitlab-saas master
+git merge --ff-only ci/drone master
 
-git merge master ci/github-actions
-git merge master ci/gitlab-saas
-git merge master ci/drone
+git fetch . master:ci/github-actions
+git fetch . master:ci/gitlab-saas
+git fetch . master:ci/drone
 
 echo "+ pushing refs to github"
 # git push github --all
